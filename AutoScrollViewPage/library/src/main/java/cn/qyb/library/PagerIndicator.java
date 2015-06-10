04c78a7ddf897e9bd1c844e0a6b367ac035e4ac9
qiaoyubo <qyb0010@hotmail.com>
2015-06-10 23:59:30 +0800

    change

--- AutoScrollViewPage/library/src/main/java/cn/qyb/library/PagerIndicator.java
+++ AutoScrollViewPage/library/src/main/java/cn/qyb/library/PagerIndicator.java
@@ -1,15 +1,18 @@
 package cn.qyb.library;
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 /**
@@ -17,7 +20,6 @@ import android.widget.LinearLayout;
  */
 public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChangeListener{
 
-    private static final int DEFAULT_SIZE = 100;
     private static final int DEFAULT_INDICATOR_RADIUS = 20;
     private static final int DEFAULT_SPACE = 20;
 
@@ -29,6 +31,12 @@ public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChan
     private int mCurrentItem = 0;
     private int mTotalCount;
 
+    private int mAnimationInResId;
+    private int mAnimationOutResId;
+
+    private int mSelectedDrawableResId;
+    private int mUnSelectedDrawableResId;
+
     private Animator mAnimationIn;
     private Animator mAnimationOut;
 
@@ -38,24 +46,31 @@ public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChan
 
     public PagerIndicator(Context context, AttributeSet attrs) {
         super(context, attrs);
+        if (attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PagerIndicator);
+            mIndicatorRadius = typedArray.getDimensionPixelOffset(R.styleable.PagerIndicator_radius, DEFAULT_INDICATOR_RADIUS);
+            mSpace = typedArray.getDimensionPixelOffset(R.styleable.PagerIndicator_space, DEFAULT_SPACE);
+            mAnimationInResId = typedArray.getResourceId(R.styleable.PagerIndicator_animatorIn, 0);
+            mAnimationOutResId = typedArray.getResourceId(R.styleable.PagerIndicator_animatorOut, 0);
+            mSelectedDrawableResId = typedArray.getResourceId(R.styleable.PagerIndicator_drawableSelected, R.drawable.indicator_selected);
+            mUnSelectedDrawableResId = typedArray.getResourceId(R.styleable.PagerIndicator_drawableUnselected, R.drawable.indicator_unselected);
+            // use nineoldandroids
+//            if (mAnimationInResId != 0) {
+//                mAnimationIn = AnimatorInflater.loadAnimator(context, mAnimationInResId);
+//            }
+        }
         init();
     }
 
     private void init() {
         setOrientation(HORIZONTAL);
         setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
-        configIndicator();
 //        initAnim();
     }
 
 //    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 //    private void initAnim() {
 //    }
-
-    private void configIndicator() {
-        mIndicatorRadius = DEFAULT_INDICATOR_RADIUS;
-        mSpace = DEFAULT_SPACE;
-    }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -71,12 +86,12 @@ public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChan
 //            mAnimationOut.end();
 //        }
         View preIndicator = getChildAt(mCurrentItem % mTotalCount);
-        preIndicator.setBackgroundResource(R.drawable.indicator_unselected);
+        preIndicator.setBackgroundResource(mUnSelectedDrawableResId);
 //        mAnimationOut.setTarget(preIndicator);
 //        mAnimationOut.start();
 
         View selectedIndicator = getChildAt(position);
-        selectedIndicator.setBackgroundResource(R.drawable.indicator_selected);
+        selectedIndicator.setBackgroundResource(mSelectedDrawableResId);
 //        mAnimationIn.setTarget(selectedIndicator);
 //        mAnimationIn.start();
 
@@ -101,9 +116,9 @@ public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChan
     private void createIndicator() {
         removeAllViews();
         if (mTotalCount > 1) {
-            addIndicator(R.drawable.indicator_selected);
+            addIndicator(mSelectedDrawableResId);
             for (int i = 1; i < mTotalCount; i++) {
-                addIndicator(R.drawable.indicator_unselected);
+                addIndicator(mUnSelectedDrawableResId);
             }
         }
     }
@@ -117,5 +132,10 @@ public class PagerIndicator extends LinearLayout implements ViewPager.OnPageChan
         params.rightMargin = mSpace / 2;
         indicator.setLayoutParams(params);
     }
+
+    public void setRadius(int radius) {
+        mIndicatorRadius = radius;
+    }
+
 
 }
